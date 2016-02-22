059e139f7d97cf1d89e0179be4380b692b6c25c3
Nicholas Gill <nicholas.gill23@gmail.com>
2016-02-22 15:46:23 -0600

    updated the look of the modal for books

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -17,9 +17,30 @@ myApp.controller('categoryCtrl', function($scope, $http, $routeParams){
     $scope.categorys = [];
     $http.get('/data/').then(function (res) {
         $scope.categorys = res.data;
+
+        window.setTimeout(function() {
+            $('.js-modal').on('click', function(){
+                var $modal = $('.modal');
+                var $modalInner = $modal.find('.modal-inner');
+                var bookIndex = $(this).index();
+                var categorynum = $('.js-category').data('categorynum');
+                var category = $scope.category.books[bookIndex];
+                var source = $('.modalTemplate').html();
+                var template = Handlebars.compile(source);
+                var html = template(category);
+                $modalInner.html(html);
+                $modal.show();
+            });
+
+            $('.modal').on('click', function(){
+                $('.modal').hide();
+            });
+        }.bind(this), 500);
+
         for (var i = 0; i < $scope.categorys.length; i++) {
             if ($scope.categorys[i].tag === $routeParams.tag) {
                 $scope.category = $scope.categorys[i];
+                $scope.categorynum = i;
                 break;
 
             }
