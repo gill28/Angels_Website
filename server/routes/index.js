884c2afd65a75a50cba58a3b4162069a0b6368b6
Nicholas Gill <nicholas.gill23@gmail.com>
2016-03-02 12:32:01 -0600

    implementing nodeMailer

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var nodemailer = require('nodemailer');
+var app = express();
 
 router.get('/', function(req, res, next){
     var index = path.join(__dirname, "../../public/main.html");
